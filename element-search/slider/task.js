9d7dfcfc8fce4194eba3e4e52af15ab12733751d
Alexandra_Ignatenko <9val_2004@mail.ru>
2023-03-15 17:59:15 +1000

    задача 3

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,12 +1,20 @@
-const items = Array.from(document.querySelectorAll(".slider__item"));
 const prev = document.querySelector(".slider__arrow_prev");
 const next = document.querySelector(".slider__arrow_next");
-let slideId = items.findIndex(item => item.classList.contains(activeClassName));;
 
-next.onclick = () => {
+function changeSlide() {
+  const items = Array.from(document.getElementsByClassName("slider__item"));
+  const activeItem = document.querySelector(".slider__item_active");
+  const itemIndex = items.findIndex(item => item === activeItem);
+  const navNext = this.className.includes("next") ? 1 : -1;
+  let index = itemIndex + navNext;
   
-};
+  if (index < 0) {
+      index = items.length - 1;
+  } else if (index > items.length - 1) {
+      index = 0;
+  }
+  items[index].className += " slider__item_active";
+}
 
-prev.onclick = () => {
- 
-};
+prev.onclick = changeSlide;
+next.onclick =changeSlide;
